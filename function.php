c401d3eef7d6726895b2be62de73a3a84891176f
Anjelina9699 <71862871+Anjelina9699@users.noreply.github.com>
2021-10-18 21:56:02 +0700

    Tugas (Pertemuan 5)

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Anjelina Donda";
+$umur = 21;
+$asalSekolah ='SMK Negeri 1 Ende';
+$semester = 7;
+$jurusan ="Sistem Informasi";
+
+	echo "Hello, perkenalkan nama saya ".$nama.", umur
+		  saya ".$umur." tahun. Sekarang saya kuliah di ".
+		  $asalPerguruanTinggi.", semester ".$semester." jurusan ".$jurusan;
+	?>
